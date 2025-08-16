2fae285f5ec92dc1729e7287f49f8c0cda306900
NabhaGK <69899310+NabhaGK@users.noreply.github.com>
2025-08-17 00:38:37 +0530

    Update streamlit_app.py
    Made request output readable by extracting it in json format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,6 @@ if ingredients_list:
 #New section to display nutritional info
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
